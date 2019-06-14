708465a36446d44df6e63ea351685264d169cda4
Jullierme Silva Barros <julliermegyn@gmail.com>
2019-06-14 17:51:57 -0300

    more configuration

--- config/webpack.base.config.js
+++ config/webpack.base.config.js
@@ -1,34 +1,53 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin')
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const devMode = process.env.NODE_ENV !== 'production';
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const devMode = process.env.NODE_ENV !== 'production'
+const path = require('path')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 
 module.exports = {
+  devtool: 'cheap-module-eval-source-map',
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, '../dist'),
+    filename: '[name].[hash].js',
+    chunkFilename: '[id].[hash].js',
+    publicPath: '',
+  },
   module: {
     rules: [
       {
         test: /\.js$/,
         exclude: /node_modules/,
         use: {
-          loader: 'babel-loader'
-        }
+          loader: 'babel-loader',
+        },
       },
       {
         test: /\.scss$/,
+        exclude: /node_modules/,
         use: [
           {
-            loader: "style-loader" // creates style nodes from JS strings
+            loader: 'style-loader', // creates style nodes from JS strings
           },
           {
-            loader: "css-loader" // translates CSS into CommonJS
+            loader: 'css-loader', // translates CSS into CommonJS
           },
           {
-            loader: "sass-loader" // compiles Sass to CSS
-          }
+            loader: 'postcss-loader', // autoprefixer
+          },
+          {
+            loader: 'sass-loader', // compiles Sass to CSS
+          },
         ],
-      }
-    ]
+      },
+      {
+        test: /\.(png|jpe?g|gif|svg)$/,
+        loader: 'url-loader?limit=10000&name=images/[name].[ext]' /* 10 kb */,
+      },
+    ],
   },
   plugins: [
+    new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
       template: './src/index.html',
       filename: './index.html',
@@ -40,5 +59,5 @@ module.exports = {
       filename: devMode ? '[name].css' : '[name].[hash].css',
       chunkFilename: devMode ? '[id].css' : '[id].[hash].css',
     }),
-  ]
+  ],
 }
--- src/index.js
+++ src/index.js
@@ -2,17 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.scss'
 import registerServiceWorker from './registerServiceWorker'
-
-export const App = () => (
-  <div className="App">
-    <header className="App-header">
-      <h1 className="App-title">Welcome to React!</h1>
-    </header>
-    <p className="App-intro">
-            To get started, edit <code>src/index.js</code> and save to reload...
-    </p>
-  </div>
-)
+import App from './App'
 
 ReactDOM.render(<App />, document.getElementById('root'))
 registerServiceWorker()
